1c7fceddbfdd7b7eb45be7543cdfb7e7282c319d
Kunj <kunjmaniar1999@gmail.com>
2021-01-10 18:20:07 +0530

    heroku test 2

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     production:{
         SECRET: process.env.SECRET,
-        DATABASE: process.env.MONGODB_URI,
+        DATABASE: process.env.DATABASE,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         URI: process.env.URI
@@ -11,7 +11,7 @@ const config = {
         DATABASE: 'mongodb+srv://Kunj:Zluri@Contacts@cluster0.6b07i.mongodb.net/contacts?retryWrites=true&w=majority',
         GOOGLE_CLIENT_ID: '880916036413-7cakj7fghuhtoupfhns0l321paglaoj6.apps.googleusercontent.com',
         GOOGLE_CLIENT_SECRET: 'TVi-uDcBdyv3-tCH-LOCyEKZ',
-        URI: "http://localhost:3001"
+        URI: "http://localhost:5000"
     }
 }
 
